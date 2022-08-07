26b5421a47b9438381d0cc84bc908dadfa0255e2
= <=>
2022-08-07 13:55:16 -0400

    added components

--- frontend/src/pages/list/List.jsx
+++ frontend/src/pages/list/List.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import "./list.css"
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 
 export default function List() {
   return (
     <div><Navbar/>
-    <Header type="list"/></div>
+    <Header type="list"/>
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label>Destination</label>
+              <input type="text" />
+            </div>
+
+            <div className="lsItem">
+              <label>Check-In Date</label>
+              <input type="text" />
+            </div>
+
+
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
+    </div>
   )
 }
